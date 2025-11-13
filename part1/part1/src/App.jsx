6d325803ac8878868ce52389999f17fa5b0f36cd
Kefirrro <wiktorfiedorek0@gmail.com>
2025-11-13 13:29:16 +0100

    part1a

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,9 +1,44 @@
-const App = () => {
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+}
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <p>Hello world</p>
+      {parts.map(part => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
-  )
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+}
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+    </div>
+  );
 }
 
 export default App
